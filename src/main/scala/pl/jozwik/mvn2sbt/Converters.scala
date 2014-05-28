4d3886aa9018fb3427a6adc79fc70053223f3b11
ajozwik <jozwik@onet.pl>
2014-05-28 17:01:11 +0200

    more options extracted from pom

--- src/main/scala/pl/jozwik/mvn2sbt/Converters.scala
+++ src/main/scala/pl/jozwik/mvn2sbt/Converters.scala
@@ -1,35 +1,55 @@
 package pl.jozwik.mvn2sbt
 
-import org.maven.Plugin
+import org.maven.{Configuration4, Plugin}
 import scala.xml.Node
 
 
 object Converters {
   type Converter = Plugin => Seq[String]
 
-  def cxfConverter:Converter = plugin => {
+  def cxfConverter: Converter = plugin => {
     val execution = plugin.executions.get.execution
-    val wsdlOptionSeq = execution.flatMap { ex =>
-      val configuration4 = ex.configuration.get
-      configuration4.any.find { r =>
-        r.key == Some("wsdlOptions")
-      } match{
-        case Some(wsdlOptions) => extract(wsdlOptions.value.asInstanceOf[Node])
-        case _ => Nil
-      }
+    val configuration4 = execution.map { ex =>
+      ex.configuration.get
+    }
+    val confHead = configuration4.head
+    val wsdlOptionSeq = extractElement(confHead, "wsdlOptions") match {
+      case Some(wsdlOptions) => extract(wsdlOptions.value.asInstanceOf[Node])
+      case _ => Nil
+    }
 
+    val wsdls = wsdlOptionSeq.map {
+      case (wsdl, packageName, extraOptions) =>
+        val pn = extraOptions ++ {
+          if (packageName.nonEmpty) {
+            Seq("-p", packageName)
+          } else Nil
+        }
+
+        s"""cxf.Wsdl("$wsdl", Seq(${pn.mkString("\"", "\",\"", "\"")}), None)"""
     }
-    val wsdls = wsdlOptionSeq.map{ case(wsdl,packageName) =>
-     s"""cxf.Wsdl("$wsdl", Seq("-p","$packageName"), None)"""
-    }
-    Seq(s"""cxf.wsdls :=Seq(${wsdls.mkString(",")})""")
+
+    Seq( s"""cxf.wsdls :=Seq(${wsdls.mkString(",")})""")
   }
 
-  private def extract(elem: Node) = {
-    (elem \ "wsdlOption").map(w => ((w \ "wsdl").text, (w \ "packagenames" \ "packagename").text))
+  def extractElement(confHead: Configuration4, name: String) = {
+    confHead.any.find { r =>
+      r.key == Some(name)
+    }
   }
 
-  def thriftConverter:Converter = plugin => Nil
+  private def extractNode(elem: Node, first: String, names: String*) = {
+    names.foldLeft(elem \ first)((acc, name) => acc \ name)
+  }
 
-  def groovyConverter:Converter = plugin => Nil
+
+  private def extract(elem: Node, names: String*) = {
+    val wsdlOption = extractNode(elem, "wsdlOption")
+    wsdlOption.map(w => (extractNode(w, "wsdl").text, extractNode(w, "packagenames", "packagename").text,
+      extractNode(w, "extraargs", "extraarg").map{_.text}))
+  }
+
+  def thriftConverter: Converter = plugin => Nil
+
+  def groovyConverter: Converter = plugin => Nil
 }
