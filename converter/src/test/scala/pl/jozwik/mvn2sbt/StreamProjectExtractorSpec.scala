38532061548d72920395de20d1716f00cd4c62fa
ajozwik <jozwik@onet.pl>
2014-06-20 23:42:33 +0200

    Wrong scope test

---
+++ converter/src/test/scala/pl/jozwik/mvn2sbt/StreamProjectExtractorSpec.scala
@@ -0,0 +1,16 @@
+package pl.jozwik.mvn2sbt
+
+
+class StreamProjectExtractorSpec extends AbstractSpec {
+
+  "StreamProjectExtractorSpec " should {
+    "Do not support ok scope" in {
+      val notSupportedScope = "+- org.codehaus.janino:janino:ok:2.6.1:ok"
+      intercept[RuntimeException] {
+        StreamProjectExtractor.addDependency(notSupportedScope, Nil)
+      }
+    }
+  }
+
+}
+
