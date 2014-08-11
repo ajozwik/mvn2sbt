c2dcce22c0dc97fe0a3f835059f9461f081300af
ajozwik <jozwik@onet.pl>
2014-08-12 00:09:24 +0200

    empty set

--- converter/src/main/scala/pl/jozwik/mvn2sbt/Dependency.scala
+++ converter/src/main/scala/pl/jozwik/mvn2sbt/Dependency.scala
@@ -8,6 +8,6 @@ case class MavenDependency(groupId: String, artifactId: String, versionId: Strin
 
 case class Dependency(mavenDependency: MavenDependency, scope: Scope, classifierTests: Boolean = false)
 
-case class Project(projectDependency: MavenDependency, projectType: ProjectType, dependencies: Set[Dependency] = Set(), path: Option[File] = None)
+case class Project(projectDependency: MavenDependency, projectType: ProjectType, dependencies: Set[Dependency] = Set.empty, path: Option[File] = None)
 
 case class ProjectInformation(projectPath: File, parent: Option[MavenDependency], resolvers: Set[String], plugins: (PluginDescription, Plugin)*)
