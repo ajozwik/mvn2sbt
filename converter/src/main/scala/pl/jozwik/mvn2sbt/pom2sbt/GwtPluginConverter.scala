7b4425b560cc99b1646bcc98fd3cd90e6bd8eb7f
ajozwik <ajozwik@dotsystems.pl>
2014-08-18 15:45:19 +0200

    Debug information added

--- converter/src/main/scala/pl/jozwik/mvn2sbt/pom2sbt/GwtPluginConverter.scala
+++ converter/src/main/scala/pl/jozwik/mvn2sbt/pom2sbt/GwtPluginConverter.scala
@@ -8,20 +8,15 @@ import pl.jozwik.mvn2sbt.PomToSbtPluginConverter
 import scala.xml.Node
 
 
-object GwtPluginConverter {
-  final val DEFAULT_GWT_VERSION = "2.3.0"
-}
-
 class GwtPluginConverter extends PomToSbtPluginConverter {
 
   import pl.jozwik.mvn2sbt.PluginConverter._
 
   protected [pom2sbt] def configurationToSet(confHead: Configuration4, rootDir: File)(implicit plugin:Plugin): Set[String] = {
-    import GwtPluginConverter._
-    val version = plugin.version.getOrElse(DEFAULT_GWT_VERSION)
+    val versionOption = plugin.version.fold(Set.empty[String])(version =>Set(s"""gwtVersion := "$version" """))
     val moduleOption = toOption(confHead,"module",s => s"""gwtModules := List("$s")""" )
     val extraArgsOption = toOption(confHead,"extraJvmArgs",s => s"""javaOptions in Gwt += "$s"""" )
-    extraArgsOption ++ moduleOption ++ Set( s"""gwtVersion := "$version" """)
+    extraArgsOption ++ moduleOption ++ versionOption
   }
 
 
