bdd98c89cc51b23c6056f2a04430add4e43d88ff
ajozwik <ajozwik@dotsystems.pl>
2014-08-18 15:59:08 +0200

    Remove default option - default is added by plugin

--- converter/src/main/scala/pl/jozwik/mvn2sbt/pom2sbt/GwtPluginConverter.scala
+++ converter/src/main/scala/pl/jozwik/mvn2sbt/pom2sbt/GwtPluginConverter.scala
@@ -13,16 +13,16 @@ class GwtPluginConverter extends PomToSbtPluginConverter {
   import pl.jozwik.mvn2sbt.PluginConverter._
 
   protected [pom2sbt] def configurationToSet(confHead: Configuration4, rootDir: File)(implicit plugin:Plugin): Set[String] = {
-    val versionOption = plugin.version.fold(Set.empty[String])(version =>Set(s"""gwtVersion := "$version" """))
+    val versionOption = plugin.version.map(version =>s"""gwtVersion := "$version" """)
     val moduleOption = toOption(confHead,"module",s => s"""gwtModules := List("$s")""" )
-    val extraArgsOption = toOption(confHead,"extraJvmArgs",s => s"""javaOptions in Gwt += "$s"""" )
-    extraArgsOption ++ moduleOption ++ versionOption
+    val extraArgsOption = toOption(confHead,"extraJvmArgs",s => s"""javaOptions in Gwt += "$s" """)
+    Set(extraArgsOption,moduleOption,versionOption).flatten
   }
 
 
   private def toOption(confHead: Configuration4,name:String,f:(String)=>String) = {
     val node = findElement(confHead, name)
     val moduleName = node.map(v => v.value.asInstanceOf[Node].text)
-    moduleName.fold(Set.empty[String])(s => Set(f(s)))
+    moduleName.map(s => f(s))
   }
 }
