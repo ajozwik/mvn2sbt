562f94b3e97a3bf1d980e451b94d33008e2d7d2f
ajozwik <andrzej.jozwik@gmail.com>
2026-05-12 19:05:49 +0200

    correct forAll

--- converter/src/test/scala/pl/jozwik/mvn2sbt/AbstractProjectSpec.scala
+++ converter/src/test/scala/pl/jozwik/mvn2sbt/AbstractProjectSpec.scala
@@ -60,7 +60,7 @@ abstract class AbstractProjectSpec(project: String) extends AbstractSpec {
 
     s"Create sbt file for $project with file" in {
       val rootDir = Paths.get(TestConstants.EXAMPLES_PROJECTS, project).toFile
-      val output  = new File("target", project)
+      val output  = new File(target, project)
       Mvn2Sbt.main(rootDir.getAbsolutePath, output.getAbsolutePath)
       val buildSbt = new File(output, BUILD_SBT)
       buildSbt should exist
